8a1d4bc2d93b03dfca835f6ab78e3d378469aed9
Harald Ponce de Leon <hpdl@oscommerce.com>
2015-06-01 02:23:05 +0200

    add a logout hook

---
+++ catalog/includes/apps/PayPal/Module/Hooks/Shop/Account/Logout.php
@@ -0,0 +1,19 @@
+<?php
+/**
+  * osCommerce Online Merchant
+  *
+  * @copyright Copyright (c) 2015 osCommerce; http://www.oscommerce.com
+  * @license GPL; http://www.oscommerce.com/gpllicense.txt
+  */
+
+namespace OSC\OM\Apps\PayPal\Module\Hooks\Shop\Account;
+
+class Logout
+{
+    public static function execute()
+    {
+        if (isset($_SESSION['paypal_login_access_token'])) {
+            unset($_SESSION['paypal_login_access_token']);
+        }
+    }
+}
