d613e3a47910aae8cc5c4f1f2a8a46ff5ea94465
Harald Ponce de Leon <hpdl@oscommerce.com>
2016-11-04 12:57:41 +0100

    fix param

--- catalog/includes/OSC/Apps/PayPal/PayPal/API/UserInfo.php
+++ catalog/includes/OSC/Apps/PayPal/PayPal/API/UserInfo.php
@@ -12,9 +12,9 @@ class UserInfo extends \OSC\Apps\PayPal\PayPal\APIAbstract
 {
     protected $type = 'login';
 
-    public function execute(array $extra_params = null)
+    public function execute(array $params = null)
     {
-        $this->url = 'https://api.' . ($this->server != 'live' ? 'sandbox.' : '') . 'paypal.com/v1/identity/openidconnect/userinfo/?schema=openid&access_token=' . $extra_params['access_token'];
+        $this->url = 'https://api.' . ($this->server != 'live' ? 'sandbox.' : '') . 'paypal.com/v1/identity/openidconnect/userinfo/?schema=openid&access_token=' . $params['access_token'];
 
         $response = $this->getResult($params);
 
